3fadfa17ba535a3e7136137a2323b379536f911f
GustavoRocha1008 <gustavo.alves1008@outlook.com>
2024-01-17 16:15:59 -0300

    curso de fundamentos terminado

--- Condições/Exercicio 08.py
+++ Condições/Exercicio 08.py
@@ -1,3 +1,22 @@
-#Faca um programa que leia três numeros e mostre qual é o maior e qual é o menor 
+info1 = float(input("Digite o primeiro numero: "))
+info2 = float(input("Digite o segundo numero: "))
+info3 = float(input("Digite o terceiro numero: "))
 
-entrada = int (input("Digite um numero de 1 a 5:  "))
+# Inicializando as variáveis para armazenar o maior e o menor número
+maior_numero = menor_numero = info1
+
+# Verificando o segundo número
+if info2 > maior_numero:
+    maior_numero = info2
+elif info2 < menor_numero:
+    menor_numero = info2
+
+# Verificando o terceiro número
+if info3 > maior_numero:
+    maior_numero = info3
+elif info3 < menor_numero:
+    menor_numero = info3
+
+# Exibindo os resultados
+print(f"O maior número é: {maior_numero}")
+print(f"O menor número é: {menor_numero}")
